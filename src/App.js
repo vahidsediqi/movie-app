8c8bcb4dc50d81cf9547620a498029c3c0089c31
unknown <you@example.com>
2021-11-29 13:05:22 +0300

    https

--- src/App.js
+++ src/App.js
@@ -10,23 +10,21 @@ import SearchBox from './components/SearchBox';
 
 const App = () => {
 
+
   const [movies, setMovies] = useState( [] );
   const [searchValue, setSearchValue] = useState('');
 
   const getMovieRequest = async (searchValue) => {
-    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=ca6be1d4`;
-
+  const url = `https://www.omdbapi.com/?s=${searchValue}&apikey=ca6be1d4`;
     const responose = await fetch(url);
     const data = await responose.json();
-    
     if(data.Search){
       setMovies(data.Search)
-    }
-        
+    }   
   }
 
   useEffect(() => {
-    getMovieRequest(searchValue);
+     getMovieRequest(searchValue);
   }, [searchValue])
 
   return (
